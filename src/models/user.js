3054708b257216beed9b2680826c035e751ed465
Felipe <felhenao@gmail.com>
2021-02-01 13:22:18 -0500

    Added endpoint to allow user to login with their existing account and reusable function to verify their credentials

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 
+
+//Hash the plain text password before saving
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -10,6 +12,7 @@ const userSchema = new mongoose.Schema({
     }, 
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -41,15 +44,27 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
 userSchema.pre('save', async function (next) {
     const user = this
-
+    
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password, 8)
     } 
-
     next()
-
 })
 
 const User = mongoose.model('User', userSchema)

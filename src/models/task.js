86b9448e68ede8aab2e0d5a177bbdde7c81d4ad3
Felipe <felhenao@gmail.com>
2021-02-02 15:24:35 -0500

    Added timestamps to task and user model

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+const TaskSchema = new mongoose.Schema({
     description: {
         type: String,
         trim: true,
@@ -16,6 +16,10 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+
+},{
+    timestamps: true
 })
 
+const Task = mongoose.model('Task', TaskSchema)
 module.exports = Task

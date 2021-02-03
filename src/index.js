afd3a4b3d7e7218bfdf2da6b2cae06c13b60852a
Felipe <felhenao@gmail.com>
2021-02-03 17:20:28 -0500

    Added auto image resizing and formating to uploads

--- src/index.js
+++ src/index.js
@@ -2,30 +2,9 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const multer = require('multer')
 
 const app = express()
-const port = process.env.PORT || 3000
-
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word document'))
-        }
-        cb(undefined, true)
-    }
-})
-
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+const port = process.env.PORT || 3000 
 
 app.use(express.json())//use incoming json
 app.use(userRouter)
